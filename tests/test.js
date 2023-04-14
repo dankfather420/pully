b4a1dfce4b8350bb0ea38b75c34193505ab0a8e2
dankfather420 <130778716+dankfather420@users.noreply.github.com>
2023-04-14 14:08:07 -0400

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,7 @@ console.log("Tests are not implemented yet...");
 
 const { exec } = require('child_process');
 
-out = exec('ls ~/work/*');
+out = exec('ls ~/work/*/*');
 out.stdout.on('data', function(data) {
     console.log(data); 
 });
